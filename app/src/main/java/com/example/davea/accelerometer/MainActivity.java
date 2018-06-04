82fb9e6e8b2fb55067003c1b7a1463499a2805e3
sheppardd1 <sheppardd1@users.noreply.github.com>
2018-06-04 17:34:47 -0500

    comment update

--- app/src/main/java/com/example/davea/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/davea/accelerometer/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         setup();    //assign and setup everything
 
+
         BtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
